e7f497167c4eba7275ca8af49f8eaa17c743a4fa
Jaqueline Agnes <jagnes@cordial.com>
2024-11-15 15:45:08 -0300

    Fix the strategy of table deletion

--- projects/zombieplus/tests/e2e/movies.spec.js
+++ projects/zombieplus/tests/e2e/movies.spec.js
@@ -2,14 +2,27 @@ const { test } = require('../support');
 import data from "../support/fixtures/movies.json";
 import { executeSQL } from "../support/database";
 
+test.beforeAll(async () => {
+    await executeSQL(`DELETE FROM public.movies`); //database table
+})
+
 test("deve poder cadastrar um novo filme", async ({ page }) => {
 
     const movie = data.create
-    await executeSQL(`DELETE FROM public.movies
-	WHERE  title = 'Guerra Mundial Z';`);
     await page.login.do("admin@zombieplus.com", "pwd123", "Admin");
-    await page.movies.create(movie.title, movie.overview, movie.company, movie.release_year);
+    await page.movies.create(movie);
     await page.toast.containText("Cadastro realizado com sucesso");
+});
+
+test("deve poder cadastrar quando um título é duplicado", async ({ page }) => {
+
+    const movie = data.duplicate  
+    await page.login.do("admin@zombieplus.com", "pwd123", "Admin");
+    await page.movies.create(movie);
+    await page.toast.containText("Cadastro realizado com sucesso");
+
+    await page.movies.create(movie);
+    await page.toast.containText("Este conteúdo já encontra-se cadastrado no catálogo");
 });
 
 test("não deve cadastrar quando os campos obrigatórios não são preenchidos", async ({page}) => {
