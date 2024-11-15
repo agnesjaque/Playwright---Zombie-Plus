99ba77b3110ac837ce8519834557e31c4c0331ad
Jaqueline Agnes <jagnes@cordial.com>
2024-11-15 15:47:12 -0300

    Fix imports for new index path

--- projects/zombieplus/tests/support/index.js
+++ projects/zombieplus/tests/support/index.js
@@ -1,9 +1,8 @@
 const { test: base} = require("@playwright/test");
-import { Login } from "../actions/Login";
-import { Toast } from "../actions/Component";
-import { Movies } from "../actions/Movies";
-import  data  from "../support/fixtures/movies.json";
-import {Leads} from "../actions/Leads"
+import { Login } from "./actions/Login";
+import { Toast } from "./actions/Component";
+import { Movies } from "./actions/Movies";
+import {Leads} from "./actions/Leads"
 
 
 const test = base.extend({
